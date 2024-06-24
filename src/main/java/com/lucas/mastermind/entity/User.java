1387a758400be93a84accac384b04e145dc5f7fc
Lucas Juszkiewicz <l.m.juszkiewicz@gmail.com>
2024-06-24 18:24:12 +0200

    First commit

---
+++ src/main/java/com/lucas/mastermind/entity/User.java
@@ -0,0 +1,42 @@
+package com.lucas.mastermind.entity;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+
+@Entity
+@Table(name = "users")
+@Data
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id")
+    @NotBlank
+    private Long id;
+
+    @Column(name = "nick")
+    @NotBlank(message = "You have to provide your Nick!")
+    private String nick;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "country")
+    private String country;
+
+    @Column(name = "password")
+    @NotBlank(message = "You have to provide your Password!")
+    private String password;
+
+    @Column(name = "games")
+    private Long games;
+
+    @Column(name = "total")
+    private Long total;
+
+    @Column(name = "img")
+    private String img;
+
+    @Column(name = "avatar")
+    private int avatar;
+}
