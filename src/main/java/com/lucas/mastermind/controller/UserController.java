eb8936f3a10868486387825687489018d7cce351
Lucas Juszkiewicz <l.m.juszkiewicz@gmail.com>
2024-06-25 15:14:49 +0200

    CRUD endpoins are made

--- src/main/java/com/lucas/mastermind/controller/UserController.java
+++ src/main/java/com/lucas/mastermind/controller/UserController.java
@@ -9,6 +9,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @AllArgsConstructor
 @RestController
 @RequestMapping("/users")
@@ -19,7 +21,24 @@ public class UserController {
     UserService userService;
 
     @PostMapping("/save")
-    public ResponseEntity<User> saveStudent(@Valid @RequestBody User user){
+    public ResponseEntity<User> saveStudent(@Valid @RequestBody User user) {
         return new ResponseEntity<>(userService.saveUser(user), HttpStatus.CREATED);
+    }
+
+    @GetMapping("/get/{userId}")
+    public ResponseEntity<User> getUser(@PathVariable Long userId) {
+        User userById = userService.getUserById(userId);
+        return new ResponseEntity<>(userById, HttpStatus.OK);
+    }
+
+    @GetMapping("/getAll")
+    public ResponseEntity<List<User>> getAllUsers() {
+        return new ResponseEntity<>(userService.getAllUsers(), HttpStatus.OK);
+    }
+
+    @DeleteMapping("/delete/{userId}")
+    public ResponseEntity<HttpStatus> deleteUserById(@PathVariable Long userId) {
+        userService.deleteUser(userId);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 }
--- src/main/java/com/lucas/mastermind/repository/UserRepository.java
+++ src/main/java/com/lucas/mastermind/repository/UserRepository.java
@@ -5,7 +5,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface UserRepository extends CrudRepository<User, Long> {
-
+    @Override
+    List<User> findAll();
 }
