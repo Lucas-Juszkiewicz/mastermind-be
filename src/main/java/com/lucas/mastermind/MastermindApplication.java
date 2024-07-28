ce91d92abc2c8d58754df48f4dfaaf61fef091d9
Lucas Juszkiewicz <l.m.juszkiewicz@gmail.com>
2024-07-28 21:47:15 +0200

    Cors problem

--- src/main/java/com/lucas/mastermind/MastermindApplication.java
+++ src/main/java/com/lucas/mastermind/MastermindApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
 
+
 @SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
 //@SpringBootApplication
 public class MastermindApplication {
@@ -12,6 +13,5 @@ public class MastermindApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(MastermindApplication.class, args);
 	}
-
 
 }
